5d9b3af4f55499e807583dbc129faa9fe5ed952c
Josh <123joshlee123@gmail.com>
2023-01-24 00:04:24 -0800

    Create basic pages and routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Login from './Login';
+import Home from './pages/Home';
+import Chats from './pages/Chats';
 // import { Register } from './Register';
 
 
 
   function App() {
     return (
-      <div className="App">
-        <Login/>
-      </div>
-    );
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="login">Login</Link>
+          <Link to="chats">Chats</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/chats" element={<Chats/>}></Route>
+        </Routes>
+      </Router>
+    )
   }
  
 
