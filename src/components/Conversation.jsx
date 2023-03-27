0c927a0812def17147780aacef70a8d9a29bbc92
Josh <123joshlee123@gmail.com>
2023-03-26 20:06:02 -0700

    Style the files and images in conversation

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -11,8 +11,11 @@ import {
   orderBy,
   limit,
 } from "@firebase/firestore";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFile } from "@fortawesome/free-regular-svg-icons";
 import UserContext from "../context/UserContext";
 import ChatInput from "./ChatInput";
+import "../styling/Chats.css";
 
 export default function Conversation({ conv }) {
   const conversation_id = conv ? conv.conversation_id : "";
@@ -47,21 +50,31 @@ export default function Conversation({ conv }) {
 
   async function sendMessage(message, files) {
     try {
-      let file_urls = [];
+      const image_urls = [];
+      const pdf_urls = [];
       if (files && files.length > 0) {
         // eslint-disable-next-line array-callback-return
         await Promise.all(
           Array.from(files).map(async (file) => {
             const fileName = `messages/${file.name}`;
             const fileRef = ref(storage, fileName);
+            const isImage = file.type.includes("image");
             await uploadBytes(fileRef, file);
             console.log("uploaded file!");
-            const url = await getDownloadURL(fileRef);
-            console.log("url:", url);
-            file_urls.push(url);
+            const urlObj = {
+              url: await getDownloadURL(fileRef),
+              type: isImage ? "image" : "pdf",
+            };
+            console.log("url:", urlObj);
+            if (isImage) {
+              image_urls.push(urlObj);
+            } else {
+              pdf_urls.push(urlObj);
+            }
           })
         );
       }
+      const file_urls = [...pdf_urls, ...image_urls];
       const newDocRef = doc(messagesRef);
       const messageData = {
         id: newDocRef.id,
@@ -84,16 +97,76 @@ export default function Conversation({ conv }) {
     );
   }
 
+  function openPdf(url) {
+    window.open(url, "_blank");
+  }
+
+  function openImage(url) {
+    // Create the modal container
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    // Create the image element
+    const img = document.createElement("img");
+    img.src = url;
+    img.classList.add("modal-content");
+
+    // Create the close button element
+    const span = document.createElement("span");
+    span.classList.add("close");
+    span.innerHTML = "&times;";
+
+    // Define the click handler for the close button
+    span.onclick = () => modal.remove();
+
+    // Append the image and close button elements to the modal container
+    modal.appendChild(span);
+    modal.appendChild(img);
+
+    // Append the modal container to the document body
+    document.body.appendChild(modal);
+
+    // Define the click handler for clicks outside of the modal
+    const handleClickOutside = (event) => {
+      if (event.target === modal) {
+        modal.remove();
+        window.removeEventListener("click", handleClickOutside);
+      }
+    };
+
+    // Define the keydown handler for the escape key
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        modal.remove();
+        window.removeEventListener("keydown", handleKeyDown);
+      }
+    };
+
+    // Add event listeners for clicks outside of the modal and for the escape key
+    window.addEventListener("click", handleClickOutside);
+    window.addEventListener("keydown", handleKeyDown);
+  }
+
   function renderFiles(file_urls) {
     return (
       <div className="files-container">
-        {file_urls.map((url) => {
+        {file_urls.map((urlObj) => {
+          const { url, type } = urlObj;
           const fileRef = ref(storage, url);
           console.log(url);
           console.log(fileRef);
-          return (
-            <div className="file-container" key={url}>
-              <img src={url} alt={fileRef ? fileRef.name : ""} />
+          return type === "image" ? (
+            <img
+              key={url}
+              src={url}
+              className="image"
+              alt={fileRef ? fileRef.name : ""}
+              onClick={() => openImage(url)}
+            />
+          ) : (
+            <div className="pdf" onClick={() => openPdf(url)}>
+              <FontAwesomeIcon icon={faFile} className="pdf-icon" />
+              {fileRef.name}
             </div>
           );
         })}
@@ -104,7 +177,7 @@ export default function Conversation({ conv }) {
   return (
     <>
       {messages.map((message) => (
-        <div key={message.id}>
+        <div key={message.id} className="message">
           <p>{message.conversation_id}</p>
           <p>{message.text}</p>
           {message.file_urls && renderFiles(message.file_urls)}
